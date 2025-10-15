5522a080993fc288d1501d9644814060a4b9e4c4
Ainee <qmalik@keyin.com>
2025-10-15 12:07:13 -0400

    next

--- Week 6 - Abstract Classes/Demo.java
+++ Week 6 - Abstract Classes/Demo.java
@@ -5,5 +5,8 @@ public class Demo {
     public static void main(String[] args) 
     {
       //  Animal a = new Animal("","",10);
+       Cat c = new Cat("Shona","Siamese",10);
+       System.out.println(c);
+
     }
 }
