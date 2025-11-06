29a54194e77ee1824ee10510e102111c68d75727
Ainee <qmalik@keyin.com>
2025-11-06 11:45:41 -0500

    Add custom exception class and enhance exception handling examples

--- Week 10 - Exception Handling/ExceptionHandling.java
+++ Week 10 - Exception Handling/ExceptionHandling.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ExceptionHandling {
     public static void main(String args[])
    {
@@ -85,40 +87,102 @@ public class ExceptionHandling {
 //      }
 //      System.out.println("Next statement..");
 
-  try{
-    	//try-block2
-        try{     
-            //try-block3
-            try{
-            	int arr[]= {1,2,3,4};
-            	/* I'm trying to display the value of
-            	 * an element which doesn't exist. The
-            	 * code should throw an exception
-            	 */
-            	System.out.println(arr[10]);
-            }catch(ArithmeticException e){
-            	System.out.print("Arithmetic Exception");
-            	System.out.println(" handled in try-block3");
-            }
-        }
-        catch(ArithmeticException e){
-           System.out.print("Arithmetic Exception");
-           System.out.println(" handled in try-block2");
-        }
-    }
-    catch(ArithmeticException e3){
-    	System.out.print("Arithmetic Exception");
-     	System.out.println(" handled in main try-block");
-    }
-    catch(ArrayIndexOutOfBoundsException e4){
-    	System.out.print("ArrayIndexOutOfBoundsException");
-     	System.out.println(" handled in main try-block");
-    }
-    catch(Exception e5){
-    	System.out.print("Exception");
-     	System.out.println(" handled in main try-block");
-     }
+//   try{
+//     	//try-block2
+//         try{     
+//             //try-block3
+//             try{
+//             	int arr[]= {1,2,3,4};
+//             	/* I'm trying to display the value of
+//             	 * an element which doesn't exist. The
+//             	 * code should throw an exception
+//             	 */
+//             	System.out.println(arr[10]);
+//             }catch(ArithmeticException e){
+//             	System.out.print("Arithmetic Exception");
+//             	System.out.println(" handled in try-block3");
+//             }
+//         }
+//         catch(ArithmeticException e){
+//            System.out.print("Arithmetic Exception");
+//            System.out.println(" handled in try-block2");
+//         }
+//     }
+//     catch(ArithmeticException e3){
+//     	System.out.print("Arithmetic Exception");
+//      	System.out.println(" handled in main try-block");
+//     }
+//     catch(ArrayIndexOutOfBoundsException e4){
+//     	System.out.print("ArrayIndexOutOfBoundsException");
+//      	System.out.println(" handled in main try-block");
+//     }
+//     catch(Exception e5){
+//     	System.out.print("Exception");
+//      	System.out.println(" handled in main try-block");
+//      }
+
+      // int age = 19;
+      // try{
+      // if(age<18)
+      //    throw new MyOwnException("Age cannot be less than 18");
+      // else
+      //    System.out.println("Input is valid");
+      // }
+      // catch(MyOwnException e)
+      // {
+      //    System.out.println(e.getMessage());
+      // }
+
+   // try{
+   //    StudentAge(19,5);
+   // }
+   
+   // catch(MyOwnException e)
+   // {
+   //    System.out.println(e.getMessage());
+   // }
+   // catch(ArithmeticException e)
+   // {
+   //     System.out.println(e.getMessage());
+   // }
+
+   Scanner scanner = new Scanner(System.in);
+   char zone;
+   int valid = 0;
+   int banned = 0;
+   String code = scanner.next();
+try {
+            zone = code.charAt(9);
+            int district = Integer.parseInt(code.substring(3, 7));
+            valid++;
+            if (zone == 'R' && district > 2000)
+               banned++;
+}
+catch (StringIndexOutOfBoundsException exception)
+{
+            System.out.println ("Improper code length: " + code);
+}
+catch (NumberFormatException exception)
+{
+            System.out.println ("District is not numeric: " + code);
+}
+
   }
+
+  public static void StudentAge(int age, int denominator) throws MyOwnException, ArithmeticException
+  {
+   int a = 15/denominator;
+   System.out.println("I am here");
+   if(age<18)
+   {
+   System.out.println("I am here 2");
+   throw new MyOwnException("Student Age cannot be less than 18");
+  
+   }
+   else
+   System.out.println("Age is valid");
+  }
+
 }
 
 
